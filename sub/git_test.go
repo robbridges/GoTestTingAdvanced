7961a7e9e9904a6bbf46807da74ea02cd860fae6
Rob Bridges <Rmb101187@gmail.com>
2023-03-13 12:26:31 -0700

    WOrking on how to run subprocesses in tests

--- sub/git_test.go
+++ sub/git_test.go
@@ -1,1 +1,32 @@
-package sub
+package sub_test
+
+import (
+	"GoTestingAdvanced/sub"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+var (
+	testHasGit bool
+)
+
+func init() {
+	if _, err := exec.LookPath("git"); err == nil {
+		testHasGit = true
+	}
+}
+
+func TestGitStatus(t *testing.T) {
+	if !testHasGit {
+		t.Skip("Git not found on local pc")
+	}
+	want := "On branch master"
+	got, err := sub.GitStatus()
+	if err != nil {
+		t.Fatalf("Gitstatus() err = %s; want nil", err)
+	}
+	if !strings.Contains(got, want) {
+		t.Errorf("GitStatus() = %s; want prefix %s", got, want)
+	}
+}
