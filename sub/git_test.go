942869c8bb774eb6b1966dc3a45d91244f5e9039
Rob Bridges <Rmb101187@gmail.com>
2023-03-14 07:14:31 -0700

    Finished up sub command testing

--- sub/git_test.go
+++ sub/git_test.go
@@ -1,7 +1,6 @@
-package sub_test
+package sub
 
 import (
-	"GoTestingAdvanced/sub"
 	"os/exec"
 	"strings"
 	"testing"
@@ -18,11 +17,32 @@ func init() {
 }
 
 func TestGitStatus(t *testing.T) {
+	// test skipped if git not found, one option
 	if !testHasGit {
-		t.Skip("Git not found on local pc")
+		t.Skip("Git command not found")
 	}
 	want := "On branch master"
-	got, err := sub.GitStatus()
+	got, err := GitStatus()
+	if err != nil {
+		t.Fatalf("Gitstatus() err = %s; want nil", err)
+	}
+	if !strings.Contains(got, want) {
+		t.Errorf("GitStatus() = %s; want prefix %s", got, want)
+	}
+}
+
+func TestGitStatusMockCommand(t *testing.T) {
+	// if our machine does not have git we are mocking an exec cmd to echo on branch master
+	if !testHasGit {
+		execCommand = func(name string, arg ...string) *exec.Cmd {
+			return exec.Command("echo", "On branch master")
+		}
+		defer func() {
+			execCommand = exec.Command
+		}()
+	}
+	want := "On branch master"
+	got, err := GitStatus()
 	if err != nil {
 		t.Fatalf("Gitstatus() err = %s; want nil", err)
 	}
